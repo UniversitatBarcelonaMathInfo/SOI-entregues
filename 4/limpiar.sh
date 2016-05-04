3ca2ed9a6b997415747d4cbfe9f0531446a1efc9
ARNAU SISTACH REINOSO <asistare7.alumnes@ub052229.mat.ub.edu>
2016-05-04 14:05:33 +0200

    Info del bash

--- 4/limpiar.sh
+++ 4/limpiar.sh
@@ -1,2 +1,9 @@
+#info: http://tldp.org/LDP/Bash-Beginners-Guide/html/sect_07_01.html
 ps aux | egrep '/(principal|segundos|minutos|horas)'
-kill $(ps aux | egrep '/(principal|segundos|minutos|horas)' | awk '{print $2}')
+if [ $? -eq 0 ]
+then
+	kill -SIGKILL $(ps aux | egrep '/(principal|segundos|minutos|horas)' | awk '{print $2}')
+	echo "eliminat"
+else
+	echo "ERROR, cap fitxer"
+fi
